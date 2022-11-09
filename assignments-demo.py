3ef6d6ae356d15b8ed6a00fbb5ebfa8b081f007e
ibrhmies <02220201044@ogr.inonu.edu.tr>
2022-11-09 23:03:12 +0300

    assignments-demo
    Python'da operatör uygulamaları

---
+++ assignments-demo.py
@@ -0,0 +1,37 @@
+a, b, c = 2, 5, 10
+
+# kullanıcıdan aldığınız iki sayıyla a,b,c toplamının farkı nedir?
+x = int(input('Birinci sayıyı giriniz:'))
+
+y = int(input('İkinci sayıyı giriniz:'))
+
+ies = (x*y)-(a+b+c)
+print(ies)
+
+# c sayısının b sayısına kalansız bölümünü bul
+print(c//b)
+
+#a,b,c toplamının mod 3 ünü bulun.
+abc = (a+b+c) % 3
+
+print(abc)
+
+#-> b nin a. kuvvetini hesaplayın
+sef = b**a
+
+print(sef)
+
+#-> c nin küpünü hesapla
+sayilar = 1, 5, 7, 10, 3
+
+a, *b, c = sayilar
+
+print(c**3)
+
+#-> b niin içindeki sayıların toplamını yazın.
+print(b[0]+b[1]+b[2])
+
+
+
+
+
